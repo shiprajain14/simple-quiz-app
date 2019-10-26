4949fe33c4e8db42f1cc102d2a43373499bf0480
olladapunaresh <51966285+olladapunaresh@users.noreply.github.com>
2019-10-26 16:04:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var stringSimilarity = require('string-similarity');
 let secureEnv = require('secure-env');
-global.env = secureEnv({secret:'mySecretPassword'});
+global.env.enc = secureEnv({secret:'mySecretPassword'});
 
 var app = express();
 
