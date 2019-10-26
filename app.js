30fbf4b3651f4681a9ddeb530785d25a9aaa90cd
Naresh Olladapu <nolladap@in.ibm.com>
2019-10-26 16:24:46 +0530

    updated files

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-var cloudant = require('cloudant');
+var cloudant = require('@cloudant/cloudant');
 var express = require('express');
 var bodyParser = require('body-parser');
 var stringSimilarity = require('string-similarity');
