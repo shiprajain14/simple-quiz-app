78a4f5fac6d23dc60179e5842a648730c6fa7c06
Naresh Olladapu <nolladap@in.ibm.com>
2019-10-29 12:57:18 +0530

    added changes in index.html and app.js

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ var responsequiz2;
 var responsequiz3;
 var responsequiz4;
 var responsequiz5;
+var area_interests;
+var org_name;
 var time;
 var url;
 var counter=0;
@@ -42,10 +44,11 @@ app.post('/quizsubmission', function(req,res){
     var quizscore = 3;
     var doc={
         _id:req.headers.host + req.url,
-        // score:quizscore,
+        org_name:req.body.org_name,
         responsequiz1: req.body.quiz_1,
         responsequiz2: req.body.quiz_2,
         responsequiz3: req.body.quiz_3,
+        area_interests: req.body.area_interests,
         // responsequiz4: req.body.quiz_4,
         // responsequiz5: req.body.quiz_5,
         time: new Date().toISOString(),
